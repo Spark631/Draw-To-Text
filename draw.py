0b8695215961493e05d5ba25af5f2abb46624690
Donny <69885780+Spark631@users.noreply.github.com>
2022-05-25 12:45:11 -0400

    Cleaned up the code

--- draw.py
+++ draw.py
@@ -1,12 +1,11 @@
 import pygame
 import sys
-import random
 
 pygame.init()
 
 width, height = 800, 800
-pygame.display.set_caption("DTT")
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("DTT")
 
 rows = 20
 columns = 20
@@ -15,13 +14,27 @@ active = True
 
 boxes = []
 drawn = False
+
+counter = 0
+
 def draw_grid(screen, width, height):
+    """
+    This draws the grid lines
+    First For loop is for the vertical lines
+    second For loop is for the horizontal lines
+    """
+    
     for x in range(0, width, 40):
-        pygame.draw.line(screen, (255, 255, 0), (x, 0), (x, height))
+        pygame.draw.line(screen, (0, 0, 0), (x, 0), (x, height))
     for y in range(0, height, 40):
-        pygame.draw.line(screen, (255, 255, 0), (0, y), (width, y))
+        pygame.draw.line(screen, (0, 0, 0), (0, y), (width, y))
 
 def draw_boxes(screen, width, height):
+    """
+    This draws the individual boxes
+    It also appends the individual boxes from top to bottom going towards the right
+    """
+    
     for i in range(rows):
         for j in range(columns):
             pygame.draw.rect(screen, (0, 255, 0), (i * 40, j * 40, 40, 40))
@@ -29,23 +42,24 @@ def draw_boxes(screen, width, height):
     return boxes
 
 def hand_draw(screen, width, height):
+    """
+    This changes the color of the clicked box to red
+    """
+    
     for box in boxes:
         if box[1] == [255, 0, 0]:
             pygame.draw.rect(screen, (255, 0, 0), box[2])
-    pass
-
-counter = 0
 
 while active:
-
+    
     cursor = pygame.mouse.get_pos()
-            
-    if drawn == False:
+    
+    if drawn == False: # Stops the green boxes from drawing again
         draw_boxes(screen, width, height)
-        draw_grid(screen, width, height)
         drawn = True
         
     hand_draw(screen, width, height)
+    draw_grid(screen, width, height)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,40 +68,35 @@ while active:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            dicty = {}
+            char_hold = {}
             if event.key == pygame.K_SPACE:
                 with open("grid.txt", "w", encoding="utf-8") as f:
                     for i, box in enumerate(boxes):
 
-                        # This is basically creating the outline of the dictionary
-                        if i < 20:
+                        if i < 20: # This is basically creating the outline of the dictionary
 
-                            # If the box is red, then we will add it to the dictionary as an " O "
-                            if box[1] == [255, 0, 0]:
-                                dicty[i] = "😈"
+                            if box[1] == [255, 0, 0]: # If the box is red, then we will add it to the dictionary as an " 😈 "
+                                char_hold[i] = "😈"
                             else:
-                                # If the box is green, then we will add it to the dictionary as an " X "
-                                dicty[i] = "😇"
+                                char_hold[i] = "😇"  # If the box is green, then we will add it to the dictionary as an " 😇 "
                         else:
-                            # This is resetting the counter so then values over 20 can be added to the dictionary
-                            if counter == 20:
+                            if counter == 20:  # This is resetting the counter so then values over 20 can be added to the dictionary
                                 counter = 0
 
-                            if box[1] == [255, 0, 0]:
-                                dicty[counter] += "😈"
+                            if box[1] == [255, 0, 0]: # This is now adding to the dictionary instead of setting it
+                                char_hold[counter] += "😈"
                             else:
-                                dicty[counter] += "😇"
+                                char_hold[counter] += "😇"
 
                             counter += 1
 
                     for i in dicty:
-                        f.write((dicty[i]) + "\n")
+                        f.write((char_hold[i]) + "\n") # prints each key on separate lines.
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             for box in boxes:
-                if pygame.Rect(box[2]).collidepoint(cursor):
-                    box[1][0] = 255
-                    box[1][1] = 0
+                if pygame.Rect(box[2]).collidepoint(cursor): # box[2] are the coordinates of the box
+                    box[1][0] = 255 # turns the green box into red
+                    box[1][1] = 0 # Removes the green value
 
     pygame.display.update()
-    pygame.display.flip()
